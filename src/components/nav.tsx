12fbb33727ed0a96340e156eaea93e8fa627edb9
Alberto García 🦊 <albertoinformatica97@gmail.com>
2023-12-02 22:06:15 +0100

    added improvements page and eslint and prettier packages

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -3,26 +3,26 @@
 import { menuRoutes } from '@/utils/constants'
 import { useSession } from 'next-auth/react'
 import Link from 'next/link'
-import { LogoutButton } from './auth/logout-button'
+import { GiMountainClimbing, GiMountaintop } from 'react-icons/gi'
 
 export const Nav = () => {
 	const { data: session } = useSession()
 
 	return (
 		<header className='bg-gradient-to-r from-amber-300 to-amber-500'>
-			<nav className='mx-auto hidden h-16 max-w-7xl items-center justify-between sm:flex'>
-				<div className='flex gap-5 space-x-9'>
+			<nav className='mx-auto hidden h-16 max-w-7xl items-center justify-between text-lg sm:flex'>
+				<div className='flex items-center gap-5 space-x-9'>
 					{menuRoutes.map(({ name, path }) => (
-						<Link key={path} href={path}>
-							{name}
+						<Link key={path} href={path} className='hover:scale-105'>
+							{path === '/' ? <GiMountaintop className='h-10 w-10' /> : name}
 						</Link>
 					))}
 				</div>
-				<Link href={'/login'}>login</Link>
 				{session?.user && (
 					<div>
-						<h1>Welcome {session.user.name}</h1>
-						<LogoutButton />
+						{/* <h1>Welcome {session.user.name}</h1> */}
+						<GiMountainClimbing className='h-10 w-10' />
+						{/* <LogoutButton /> */}
 					</div>
 				)}
 			</nav>
